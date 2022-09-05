73ee421d0285728eb6878b6db9522b467dde065d
doniurek <aldonaglebicka@gmail.com>
2022-09-05 14:55:21 +0200

    Get Global State With The useContext Hook

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,48 +1,47 @@
-import {motion, AnimatePresence} from "framer-motion"
-import PropTypes from "prop-types"
+import { motion, AnimatePresence } from "framer-motion"
+import { useContext } from "react"
 import FeedbackItem from "./FeedbackItem"
+import FeedbackContext from "./context/FeedbackContext"
 
-function FeedbackList({ feedback, handleDelete }) {
-if(!feedback || feedback.length === 0) {
-  return <p>No feedback yet</p>
-}
+function FeedbackList({ handleDelete }) {
+  const {feedback} = useContext(FeedbackContext)
 
-return (
-  <div className="feedback-list">
-    <AnimatePresence>
-      {feedback.map((item) => (
-        <motion.div
-          key={item.id}
-          initial={{opacity: 0}}
-          animate={{opacity: 1}}
-          exit={{opacity: 0}}
-        >
-          <FeedbackItem
+  if(!feedback || feedback.length === 0) {
+    return <p>No feedback yet</p>
+  }
+
+  return (
+    <div className="feedback-list">
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
             key={item.id}
-            item={item}
-            handleDelete={handleDelete}
-          />
-        </motion.div>
-      ))}
-    </AnimatePresence>
-  </div>
-)
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
+  )
 
-  // return (
-  //   <div className="feedback-list">
-  //     {feedback.map((item) => (
-  //       <FeedbackItem
-  //         key={item.id}
-  //         item={item}
-  //         handleDelete={handleDelete}
-  //       />
-  //     ))}
-  //   </div>
-  // )
-}
-
-FeedbackList.propTypes = {
-  feedback: PropTypes.array.isRequired
+    // return (
+    //   <div className="feedback-list">
+    //     {feedback.map((item) => (
+    //       <FeedbackItem
+    //         key={item.id}
+    //         item={item}
+    //         handleDelete={handleDelete}
+    //       />
+    //     ))}
+    //   </div>
+    // )
 }
 
 export default FeedbackList
